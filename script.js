6d4a95183773e193615ab08f9ed64774389bdf84
Majid Abbasi <abbasimajid788@yahoo.com>
2016-10-05 02:56:41 +0500

    Project Complete

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+var name = prompt("Enter Your Name","");
+
+ document.getElementById("sname").innerHTML = (name);
+
+
+
+
+  //var fname = prompt("Enter Father Name","");
+    //var rollper = prompt("Enter Roll No.","");
+  //var htmlper = prompt("Enter Percentage In HTMl test:","");
+  //var css1per = parseInt(prompt("Enter Percentage in CSS1 test:",""));
+ // var css2per = prompt("Enter Percentage in CSS2 test:","");
+   
+
